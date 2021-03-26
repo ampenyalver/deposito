d93e2c130b88dc3a53ee9f8eed8b0d90ef4e39a0
Ana M Peñalver <ampenyalver@gmail.com>
2021-03-26 08:50:13 +0100

    pruebas

--- DepositoCombustible/src/DepositoCombustibleTest.java
+++ DepositoCombustible/src/DepositoCombustibleTest.java
@@ -29,23 +29,42 @@ public class DepositoCombustibleTest {
 	@Test
 	public void testEstaLleno() {
 		//assertTrue(dep.estaLleno());
-		assertFalse(dep.estaLleno());
+		//assertFalse(dep.estaLleno());
+		
+		double amount=20; //mitad de capacidad
+		depNivel = depNivel + amount;
+		assertFalse(dep.estaLleno());//devuelve falso porque no esta lleno a mitad de su capacidad
 	}
 
 	@Test
 	public void testFill() {
-		double cantidad=20;
-		dep.fill(cantidad);
-		//depNivel=dep.getDepositoNivel() + cantidad;
-		assertEquals(30,dep.getDepositoNivel(),0.000001);
+		//double cantidad=20;
+		//dep.fill(cantidad);
+		//assertEquals(30,dep.getDepositoNivel(),0.000001);
+		//double cantidad2=-20;
+		//dep.fill(cantidad2);
+		//assertEquals(-10,dep.getDepositoNivel(),0.000001);
+		  double cantidad3=40;
+		  dep.fill(cantidad3);
+		  assertEquals(50,dep.getDepositoNivel(),0.000001);
+	    //depNivel=dep.getDepositoNivel() + cantidad;
+		
+		
+		//assertEquals(30,dep.getDepositoNivel(),0.000001);
 	}
 	
 	@Test
 	public void testConsumir() {
-		double gasto=10;
-		dep.consumir(gasto);
+		//double gasto=10;
+		double gasto2=-10;
+		double gasto3=30;
 		//depNivel = depNivel - gasto;
-		assertEquals(0,dep.getDepositoNivel(),0.000001);
+		//dep.consumir(gasto);
+		//assertEquals(0,dep.getDepositoNivel(),0.000001);
+		//dep.consumir(gasto2);
+		//assertEquals(20,dep.getDepositoNivel(),0.000001);
+		dep.consumir(gasto3);
+		assertEquals(-20,dep.getDepositoNivel(),0.000001);
 	}
 
 }
